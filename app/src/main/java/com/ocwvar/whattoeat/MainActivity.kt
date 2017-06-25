685af9fcad08dc078a53afd07302a17d64454cb4
ocwvar <ocwvarx2@gmail.com>
2017-06-25 22:55:46 +0800

    first commit

---
+++ app/src/main/java/com/ocwvar/whattoeat/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ocwvar.whattoeat
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Project Whattoeat
+ * Created by OCWVAR
+ * On 17-6-25 下午9:54
+ * File Location com.ocwvar.whattoeat
+ * This file use to :   主界面
+ */
+class MainActivity : AppCompatActivity(){
+}
